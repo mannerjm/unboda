dff9eaf2e4b3be45ce26f9db08e2ba8804487e04
mannerjm <mannerjm0111@gmail.com>
2026-09-18 16:02:25 +0900

    Add compatibility category entry route

---
+++ app/special-analysis/compatibility/friend/page.tsx
@@ -0,0 +1,6 @@
+import PairCompatibilityPage from "../PairCompatibilityPage";
+import { COMPATIBILITY_FRIEND_PRODUCT_ID } from "@/app/lib/specialAnalysisProducts";
+
+export default function FriendCompatibilityPage() {
+  return <PairCompatibilityPage productId={COMPATIBILITY_FRIEND_PRODUCT_ID} />;
+}

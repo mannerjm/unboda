1d30d58c580d027798a0021ad6454dbcf6d52a61
mannerjm <mannerjm0111@gmail.com>
2026-09-18 16:02:22 +0900

    Add compatibility category entry route

---
+++ app/special-analysis/compatibility/workplace/page.tsx
@@ -0,0 +1,6 @@
+import PairCompatibilityPage from "../PairCompatibilityPage";
+import { COMPATIBILITY_WORKPLACE_PRODUCT_ID } from "@/app/lib/specialAnalysisProducts";
+
+export default function WorkplaceCompatibilityPage() {
+  return <PairCompatibilityPage productId={COMPATIBILITY_WORKPLACE_PRODUCT_ID} />;
+}
